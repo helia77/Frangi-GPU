f9189a7ab9100bfb3206fed3d9630704157446ab
Helia Gohar <helia77@users.noreply.github.com>
2023-03-28 16:06:16 -0500

    Files replaced

--- frangi.cpp
+++ frangi.cpp
@@ -1,180 +1,301 @@
 #define _USE_MATH_DEFINES
-#include"frangi.h"
 #include<iostream>
+#include"tira/image.h"
 #include<cmath>
 #include<fstream>
 #include<numbers>
+#include<tira/image/colormap.h>
 
+# define PI 3.14159265358979323846  /* pi */
+const bool DEBUG = false;
+const bool USE_GPU = true;
 using namespace std;
 
+tira::image<float> convolution_gpu(tira::image<float>& src, float* kernel, int k_size);
 
-void frangi2d_hessian(const cv::Mat& src, cv::Mat& Dxx, cv::Mat& Dxy, cv::Mat& Dyy, float& scale, bool device) {
-	//construct Hessian kernels
+
+//construct Hessian kernels to later convolve with the image
+void make_HKernels(float* kern_xx_f, float* kern_xy_f, float* kern_yy_f, float scale) {
 	int half_kernel = (int)ceil(3 * scale);
 	int n_kern_x = 2 * half_kernel + 1;
 	int n_kern_y = n_kern_x;
-	float* kern_xx_f = new float[n_kern_x * n_kern_y]();
-	float* kern_xy_f = new float[n_kern_x * n_kern_y]();
-	float* kern_yy_f = new float[n_kern_x * n_kern_y]();
+
 	int i = 0, j = 0;
 	float scale_2 = scale * scale;
 	float scale_4 = scale_2 * scale_2;
 	float scale_6 = scale_2 * scale_4;
-	float PI_2 = (float)M_PI * 2.0f;
-	float half_PI_scale_4 = 1.0f / (2.0f * PI_2 * scale_4);
-	float half_PI_scale_6 = 1.0f / (2.0f * PI_2 * scale_6);
+	float half_PI_scale_4 = 1.0f / (2.0f * (float)PI * scale_4);
+	float half_PI_scale_6 = 1.0f / (2.0f * (float)PI * scale_6);
 	float exp_x2{};
 	float x2{};
 	for (int x = -half_kernel; x <= half_kernel; x++) {
 		j = 0;
 		x2 = x * x;
 		for (int y = -half_kernel; y <= half_kernel; y++) {
-			kern_xx_f[i * n_kern_y + j] = half_PI_scale_4 * (x2 / scale_2 - 1) * exp(-(x2 + y * y) / (2.0f * scale_2));
-			kern_xy_f[i * n_kern_y + j] = half_PI_scale_6 * (x * y) * exp(-(x2 + y * y) / (2.0f * scale_2));
+			float y2 = y * y;
+			kern_xx_f[i * n_kern_y + j] = half_PI_scale_4 * (x2 / scale_2 - 1) * exp(-(x2 + y2) / (2.0f * scale_2));
+			kern_xy_f[i * n_kern_y + j] = half_PI_scale_6 * (x * y) * exp(-(x2 + y2) / (2.0f * scale_2));
 			j++;
 		}
 		i++;
 	}
 
 	// kern_yy_f is transpose(kern_xx_f)
-	for (int z = 0; z < n_kern_y; z++) {
-		for (int i = 0; i < n_kern_x; i++) {
+	for (int z = 0; z < n_kern_y; z++)
+		for (int i = 0; i < n_kern_x; i++)
 			kern_yy_f[z * n_kern_x + i] = kern_xx_f[i * n_kern_x + z];
-		}
-	}
-	std::cout << "size of kern xxf: " << sizeof(kern_xx_f) << std::endl;
-	std::cout << "size of n kern x: " << sizeof(n_kern_x) << std::endl;
-	std::cout << "size of n kern y: " << sizeof(n_kern_y) << std::endl;
-	// flip kernels since kernels aren't symmetric and opencv's filter2D operation performs a correlation, not a convolution
-	cv::Mat kern_xx;
-	cv::Mat kern_xx_gpu = cv::Mat(n_kern_y, n_kern_x, CV_32FC1, kern_xx_f);
-	cv::flip(cv::Mat(n_kern_y, n_kern_x, CV_32FC1, kern_xx_f), kern_xx, -1);
 
-	cv::Mat kern_xy;
-	cv::Mat kern_xy_gpu = cv::Mat(n_kern_y, n_kern_x, CV_32FC1, kern_xy_f);
-	cv::flip(cv::Mat(n_kern_y, n_kern_x, CV_32FC1, kern_xy_f), kern_xy, -1);
+}
 
-	cv::Mat kern_yy;
-	cv::Mat kern_yy_gpu = cv::Mat(n_kern_y, n_kern_x, CV_32FC1, kern_yy_f);
-	cv::flip(cv::Mat(n_kern_y, n_kern_x, CV_32FC1, kern_yy_f), kern_yy, -1);
+//Run 2D Hessian filter with parameter sigma on src, return 3 channel image, comprising Dxx, Dxy, Dyy.
+// Host version
+tira::image<float> frangi_hessian(tira::image<float> src, float scale) {
+	
+	int half_kernel = (int)ceil(3 * scale);
+	int n_kern_x = 2 * half_kernel + 1;
+	int n_kern_y = n_kern_x;
+	float* kern_xx_f = new float[n_kern_x * n_kern_y]();				// kernel for convolving with image to create Dxx matrix
+	float* kern_xy_f = new float[n_kern_x * n_kern_y]();				// kernel for convolving with image to create Dxy matrix
+	float* kern_yy_f = new float[n_kern_x * n_kern_y]();				// kernel for convolving with image to create Dyy matrix
 
-	//specify anchor since we are to perform a convolution, not a correlation
-	cv::Point anchor(n_kern_x - n_kern_x / 2 - 1, n_kern_y - n_kern_y / 2 - 1);
-	std::cout << "size of kern xx: " << kern_xx.size() << std::endl;
-	std::cout << "size of src: " << src.size() << std::endl;
-	if (!device) {
-		cv::filter2D(src, Dxx, -1, kern_xx, anchor);
-		cv::filter2D(src, Dxy, -1, kern_xy, anchor);
-		cv::filter2D(src, Dyy, -1, kern_yy, anchor);
+	make_HKernels(kern_xx_f, kern_xy_f, kern_yy_f, scale);				// make Hessian mtx and store in (xx, xy, yy) matrices
+
+	tira::image<float> padded_img(src.border_replicate(half_kernel));		// padding the image for same size results for each scale
+
+	clock_t start, stop;
+
+	//								 -----------
+	//								| Dxx | Dxy |
+	//		Hessian					|----- -----|
+	//								| Dxy | Dyy |
+	//								 -----------
+
+	tira::image<float> Dxx, Dxy, Dyy;
+
+	if (USE_GPU) {														// convolving on GPU
+		start = clock();
+		
+		Dxx = convolution_gpu(padded_img, kern_xx_f, n_kern_x);
+		Dxy = convolution_gpu(padded_img, kern_xy_f, n_kern_x);
+		Dyy = convolution_gpu(padded_img, kern_yy_f, n_kern_x);
+
+		stop = clock();  // time finishes
+		std::cout << "Took " << (double)(stop - start) / CLOCKS_PER_SEC << " s to convolve on GPU" << std::endl;
 	}
-	else {
-		convolution_gpu(Dxx, src, kern_xx_gpu);
-		convolution_gpu(Dxy, src, kern_xy_gpu);
-		convolution_gpu(Dyy, src, kern_yy_gpu);
+	else {																// convolving on CPU
+		start = clock();
+		
+		// image(T* data, size_t x, size_t y, size_t c = 1)
+		Dxx = padded_img.convolve2(tira::image(kern_xx_f, n_kern_x, n_kern_y));
+		Dxy = padded_img.convolve2(tira::image(kern_xy_f, n_kern_x, n_kern_y));
+		Dyy = padded_img.convolve2(tira::image(kern_yy_f, n_kern_x, n_kern_y));
+
+		stop = clock();  // time finishes
+		std::cout << "Took " << (double)(stop - start) / CLOCKS_PER_SEC << " s to convolve on CPU" << std::endl;
 	}
-	std::cout << "size of Dxx: " << Dxx.size() << std::endl;
+
+	if (DEBUG) {														// check how the Dxx, Dxy, and Dyy looks
+		tira::colormap::cpu2image(Dxx.data(), "Dxx.bmp", Dxx.width(), Dxx.height());
+		tira::colormap::cpu2image(Dyy.data(), "Dyy.bmp", Dyy.width(), Dyy.height());
+		tira::colormap::cpu2image(Dxy.data(), "Dxy.bmp", Dxy.width(), Dxy.height());
+	}
+
+	// save Dxx, Dxy, Dyy as channels in one image and output
+	tira::image<float> image_3(Dxx.width(), Dxx.height(), 3);
+	image_3.channel(Dxx.data(), 0);
+	image_3.channel(Dxy.data(), 1);
+	image_3.channel(Dyy.data(), 2);
+
 	delete[] kern_xx_f;
 	delete[] kern_xy_f;
 	delete[] kern_yy_f;
+
+	return image_3;
 }
 
-void frangi2_eig2image(const cv::Mat& Dxx, const cv::Mat& Dxy, const cv::Mat& Dyy, cv::Mat& lambda1, cv::Mat& lambda2, cv::Mat& Ix, cv::Mat& Iy) {
-	//calculate eigenvectors of J, v1 and v2
-	cv::Mat tmp, tmp2;
-	tmp2 = Dxx - Dyy;
-	cv::sqrt(tmp2.mul(tmp2) + 4 * Dxy.mul(Dxy), tmp);
-	cv::Mat v2x = 2 * Dxy;
-	cv::Mat v2y = Dyy - Dxx + tmp;
+// Helper functions - multiplying all image pixels by a float - for tira::image
+tira::image<float> mult(tira::image<float> img, float n) {
 
-	//normalize
-	cv::Mat mag;
-	cv::sqrt((v2x.mul(v2x) + v2y.mul(v2y)), mag);
-	cv::Mat v2xtmp = v2x.mul(1.0f / mag);
-	v2xtmp.copyTo(v2x, mag != 0);
-	cv::Mat v2ytmp = v2y.mul(1.0f / mag);
-	v2ytmp.copyTo(v2y, mag != 0);
+	tira::image<float> out(img.width(), img.height(), img.channels());
 
-	//eigenvectors are orthogonal
-	cv::Mat v1x, v1y;
-	v2y.copyTo(v1x);
-	v1x = -1 * v1x;
-	v2x.copyTo(v1y);
+	for (int i = 0; i < img.width(); i++)
+		for (int j = 0; j < img.height(); j++)
+			for (int c = 0; c < img.channels(); c++)
+				out(i, j, c) = img(i, j, c) * n;
+	return out;
+}
 
-	//compute eigenvalues
-	cv::Mat mu1 = 0.5 * (Dxx + Dyy + tmp);
-	cv::Mat mu2 = 0.5 * (Dxx + Dyy - tmp);
+// Estimate eigenvalues from Dxx, Dxy, Dyy. Save results to lambda1, lambda2, Ix, Iy.
+tira::image<float> frangi_eig(tira::image<float> image_3) {
 
-	//sort eigenvalues by absolute value abs(Lambda1) < abs(Lamda2)
-	cv::Mat check = abs(mu1) > abs(mu2);
-	mu1.copyTo(lambda1); mu2.copyTo(lambda1, check);
-	mu2.copyTo(lambda2); mu1.copyTo(lambda2, check);
+	//2 stack of eigenvalues for each pixel
+	tira::image<float> output(image_3.width(), image_3.height(), 2);
 
-	v1x.copyTo(Ix); v2x.copyTo(Ix, check);
-	v1y.copyTo(Iy); v2y.copyTo(Iy, check);
+	for (int x = 0; x < image_3.width(); x++) {
+		for (int y = 0; y < image_3.height(); y++) {
+			// exctract the hessian matrix equivalent of each pixel
+			float dxx = image_3(x, y, 0);
+			float dxy = image_3(x, y, 1);
+			float dyy = image_3(x, y, 2);
+
+			// calculate eigenvectors
+			float tmp = dxx - dyy;
+			float tmp2 = sqrt(tmp * tmp + (4 * dxy * dxy));
+
+			// compute eigenvalues (calculation in presentation)
+			float mu1 = 0.5 * (dxx + dyy + tmp2);
+			float mu2 = 0.5 * (dxx + dyy - tmp2);
+
+			// sort eigenvalues and eigenvectors by absolute value abs(lambda1) < abd(lambda2)
+			float lambda1 = (abs(mu1) < abs(mu2)) ? mu1 : mu2;
+			float lambda2 = (abs(mu1) < abs(mu2)) ? mu2 : mu1;
+
+			output(x, y, 0) = lambda1;
+			output(x, y, 1) = lambda2;
+		}
+	}
+
+	std::cout << "Eigenvalue decomposition done." << std::endl;
+	return output;
+}
+
+// Comute the first component of the vesselness equation (mentioned in presentation)
+tira::image<float> term1(tira::image<float> eigs, float beta) {
+
+	tira::image<float> tmp1(eigs.width(), eigs.height());
+
+	for (int i = 0; i < eigs.width(); i++) {
+		for (int j = 0; j < eigs.height(); j++) {
+			float lambda1 = eigs(i, j, 0);
+			float lambda2 = eigs(i, j, 1);
+
+			float Rb = lambda1 / lambda2;
+
+			tmp1(i, j) = exp(-Rb * Rb / beta);
+		}
+	}
+
+	return tmp1;
+}
+
+tira::image<float> term2(tira::image<float> eigs, float c) {
+
+	tira::image<float> tmp2(eigs.width(), eigs.height());
+
+	for (int i = 0; i < eigs.width(); i++) {
+		for (int j = 0; j < eigs.height(); j++) {
+			float lambda1 = eigs(i, j, 0);
+			float lambda2 = eigs(i, j, 1);
+
+			float S2 = (lambda1 * lambda1) + (lambda2 * lambda2);
+
+			tmp2(i, j) = exp(-S2 / c);
+		}
+	}
+
+	return tmp2;
+}
+
+tira::image<float> filtered_img(tira::image<float> tmp1, tira::image<float> tmp2) {
+	
+	tira::image<float> output(tmp1.width(), tmp1.height());
+	//cv::Mat Ifiltered = tmp1.mul(cv::Mat::ones(src.rows, src.cols, src.type()) - tmp2);
+	for (int i = 0; i < tmp1.width(); i++) {
+		for (int j = 0; j < tmp1.height(); j++) {
+			output(i, j) = tmp1(i, j) * (1.0f - tmp2(i, j));
+		}
+	}
+	std::cout << "Vesselness function done." << std::endl;
+	return output;
+}
+
+// check if no lambda2 is equal to 0, if yes, given the smallest value possible
+void check_zero(tira::image<float>& eig_2) {
+
+	for (int i = 0; i < eig_2.width(); i++)
+		for (int j = 0; j < eig_2.height(); j++)
+			if (eig_2(i, j, 1) == 0)				
+				eig_2(i, j, 1) = nextafterf(0, 1);
 
 }
 
+void neg_lambda(tira::image<float>& img, tira::image<float> lambda2) {
 
-void frangi2d(const cv::Mat& src, cv::Mat& maxVals, cv::Mat& whatScale, cv::Mat& outAngles, float beta, float c, float start, float end,
-	float step, bool black, bool device) {
-	vector<cv::Mat> ALLfiltered;
-	vector<cv::Mat> ALLangles;
+	for (int i = 0; i < img.width(); i++)
+		for (int j = 0; j < img.height(); j++)
+			if(lambda2(i, j) > 0)
+				img(i, j) = 0;
+}
 
-	beta = 2 * beta * beta;
-	c = 2 * c * c;
-	int steps = (end - start) / step;
-	for (int i{}; i < steps; i++) {
+// Apply full Frangi filter to src.Vesselness is saved in J, scale is saved to scale, vessel angle is saved to directions.
+tira::image<float> frangi(tira::image<float> src, float B, float C, float start, float end,
+	float step) {
+
+	vector<tira::image<float>> ALLfiltered;
+	//vector<float> ALLangles;
+
+	float beta = 2 * B * B;			// based on the formula
+	float c = 2 * C * C;			// based on the formula
+	int cnt = 1;
+
+	for (float scale = start; scale <= end; scale += step) {
+		std::cout << "\nScale #" << cnt << std::endl;
 		//create 2D hessians
-		float scale = (i * steps + start);
-		cv::Mat Dxx, Dyy, Dxy;
-		frangi2d_hessian(src, Dxx, Dxy, Dyy, scale, device);
-		//correct for scale
-		Dxx = Dxx * scale * scale;
-		Dyy = Dyy * scale * scale;
-		Dxy = Dxy * scale * scale;
+		tira::image<float> D = frangi_hessian(src, scale);
+		
+		//correct for scale - D * scale * scale
+		D = mult(mult(D, scale), scale);
 
 		//calculate (abs sorted) eigenvalues and vectors
-		cv::Mat lambda1, lambda2, vx, vy;
-		frangi2_eig2image(Dxx, Dxy, Dyy, lambda1, lambda2, vx, vy);
+		tira::image<float> eig_2 = frangi_eig(D);
+		
+		// Check lambda2 > 0 and if equal to zero, make the smallest floating point value possible
+		check_zero(eig_2);
+		
+		// Create the two terms in the vesselness equation
+		tira::image<float> tmp1 = term1(eig_2, beta);
+		tira::image<float> tmp2 = term2(eig_2, c);
 
-		//compute direction of the minor eigenvector
-		cv::Mat angles;
-		cv::phase(vx, vy, angles);				//calculates the rotation angle of 2D vector that is formed from the corresponding elements
-		ALLangles.push_back(angles);
+		// Compute filtered image
+		tira::image<float> filtered1 = filtered_img(tmp1, tmp2);
 
-		//compute some similarity measures
-		lambda2.setTo(nextafterf(0, 1), lambda2 == 0);
-		cv::Mat Rb = lambda1.mul(1.0 / lambda2);
-		Rb = Rb.mul(Rb);
-		cv::Mat S2 = lambda1.mul(lambda1) + lambda2.mul(lambda2);
-
-		//compute output image
-		cv::Mat tmp1, tmp2;
-		cv::exp(-Rb / beta, tmp1);
-		cv::exp(-S2 / c, tmp2);
-
-		cv::Mat Ifiltered = tmp1.mul(cv::Mat::ones(src.rows, src.cols, src.type()) - tmp2);
-
-		if (black)
-			Ifiltered.setTo(0, lambda2 > 0);
-		else
-			Ifiltered.setTo(0, lambda2 < 0);
+		neg_lambda(filtered1, eig_2.channel(1));
 
 		//store results
-		ALLfiltered.push_back(Ifiltered);
+		ALLfiltered.push_back(filtered1);
+		std::cout << "Filter number " << std::to_string(cnt) << ": \n\t min: " << filtered1.minv() << "\t max: " << filtered1.maxv() << std::endl;
+		cnt++;
 	}
 
-	float s = start;
-	ALLfiltered[0].copyTo(maxVals);
-	ALLfiltered[0].copyTo(whatScale);
-	ALLfiltered[0].copyTo(outAngles);
-	whatScale.setTo(s);
+	tira::image<float> maxVals = ALLfiltered[0];
+	float max_v = 0;
+	int b = ALLfiltered.size() - 1;
+
+	tira::image<float> output_image(maxVals.width(), maxVals.height());
 
 	//find element-wise maximum across all accumulated filter results
-	for (int i = 1; i < ALLfiltered.size(); i++) {
-		maxVals = max(maxVals, ALLfiltered[i]);
-		whatScale.setTo(s, ALLfiltered[i] == maxVals);
-		ALLangles[i].copyTo(outAngles, ALLfiltered[i] == maxVals);
-		s += step;
-	}
+	for (int x = 0; x < maxVals.width(); x++)
+		for (int y = 0; y < maxVals.height(); y++) {
+			max_v = maxVals(x, y);
+			for (int i = 1; i < ALLfiltered.size(); i++)
+				if (ALLfiltered[i](x, y) > max_v)
+					max_v = ALLfiltered[i](x, y);
+
+			output_image(x, y) = max_v;
+		}
+	
+	//return mult(output_image, 255);
+	return output_image;
+}
+
+
+int main() {
+
+    float beta = 1, c = 20, step = 0.5, start = 1, end = 3;
+
+    tira::image<float> loaded_image("brain.ppm");
+	tira::image<float> img = loaded_image.channel(0);
+	tira::image<float> output_img = frangi(img, beta, c, start, end, step);
+	tira::colormap::cpu2image(output_img.data(), "result.bmp", output_img.width(), output_img.height(),
+		tira::colormap::cmGrayscale);
 }
---
+++ gpu.cu
@@ -0,0 +1,103 @@
+﻿#include<iostream>
+#include"cuda_runtime.h"
+#include<cuda_runtime_api.h>
+#include"device_launch_parameters.h"
+#include"tira/image.h"
+#include<tira/image/colormap.h>
+//#include"frangi.h"
+
+#define _USE_MATH_DEFINES
+#define __CUDACC__
+# define PI 3.14159265358979323846  /* pi */
+
+static void HandleError(cudaError_t err, const char* file, int line) {
+    if (err != cudaSuccess) {
+        std::cout << cudaGetErrorString(err) << "in" << file << "at line" << line;
+    }
+}
+#define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ )) 
+
+
+//  convolution on device
+__global__ void dev_conv(float* out, float* img, float* kernel, int img_w, int out_h, int out_w, int K) {
+    size_t i = blockDim.y * blockIdx.y + threadIdx.y;
+    size_t j = blockDim.x * blockIdx.x + threadIdx.x;
+    
+    // i and j being smaller than output's width and height, manage the edges perfectly
+    if (i >= out_h || j >= out_w) return;
+
+    float conv = 0;
+    for (int ki = 0; ki < K; ki++)
+        for (int kj = 0; kj < K; kj++)
+            conv += img[(i + ki) * img_w + j + kj] * kernel[ki*K + kj];
+        
+    out[i * out_w + j] = conv;
+
+}
+
+// convolving a kernel with an image using GPU
+tira::image<float> convolution_gpu(tira::image<float>& img, float* kernel, int k_size) {
+
+    tira::image<float> src(img);
+    int size = src.width() * src.height();		        // size of the image
+    
+    // output sizes after convolution
+    int y_height = src.height() - k_size + 1;
+    int y_width = src.width() - k_size + 1;
+    int y_size = y_height * y_width;
+    float* y_output = (float*)malloc(y_size * sizeof(float));
+
+    // -------------------------------------- GPU ---------------------------------------- //
+    int d;
+    HANDLE_ERROR(cudaGetDevice(&d));
+    //std::cout << "Current device: " << d << std::endl;
+    cudaDeviceProp prop;
+    HANDLE_ERROR(cudaGetDeviceProperties(&prop, d));
+    
+    float* gpu_gKernel;
+    float* gpu_image;
+    float* gpu_output_x;
+    float* gpu_output_y;
+    
+    // allocate memory for image, kernel, and convoled output
+    HANDLE_ERROR(cudaMalloc(&gpu_gKernel, k_size * k_size * sizeof(float)));
+    HANDLE_ERROR(cudaMalloc(&gpu_image, size * sizeof(float)));
+    HANDLE_ERROR(cudaMalloc(&gpu_output_y, y_size * sizeof(float)));
+
+    float* imageArr = img.data();
+    float* gKernel = kernel;
+
+    // copy image and kernel from main memory to Device
+    HANDLE_ERROR(cudaMemcpy(gpu_image, imageArr, size * sizeof(float), cudaMemcpyHostToDevice));
+    HANDLE_ERROR(cudaMemcpy(gpu_gKernel, gKernel, k_size * k_size * sizeof(float), cudaMemcpyHostToDevice));
+
+
+    size_t blockDim = sqrt(prop.maxThreadsPerBlock);
+    dim3 threads(blockDim, blockDim);
+    //std::cout << "w: " << width << std::endl << "h: " << height << std::endl;
+    dim3 blocks(src.width() / threads.x + 1, src.height() / threads.y + 1);
+
+    // starting GPU timer
+    /*cudaEvent_t g_start;
+    cudaEvent_t g_stop;
+    cudaEventCreate(&g_start);
+    cudaEventCreate(&g_stop);
+    cudaEventRecord(g_start, NULL);*/
+
+    // convolving
+    dev_conv << < blocks, threads >> > (gpu_output_y, gpu_image, gpu_gKernel, src.width(), y_height, y_width, k_size);
+    
+    // copy back the results to main memory
+    HANDLE_ERROR(cudaMemcpy(y_output, gpu_output_y, y_size * sizeof(float), cudaMemcpyDeviceToHost));
+
+    // GPU timer ends
+    /*cudaEventRecord(g_stop, NULL);
+    cudaEventSynchronize(g_stop);
+    float eTime;
+    cudaEventElapsedTime(&eTime, g_start, g_stop);
+    std::cout << "Takes " << eTime << " ms to convolve on GPU" << std::endl;*/
+
+    tira::image<float> output(y_output, y_width, y_height);
+    
+    return output;
+}
